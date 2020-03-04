172d6c94d1f959b5c32696c13aa1c624d766abec
abo <huangshengbo@gmail.com>
2020-03-04 14:02:28 +0800

    init commit

---
+++ Formula/gradle.rb
@@ -0,0 +1,20 @@
+class Gradle < Formula
+  desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-6.2.1-all.zip"
+  sha256 "49fad5c94e76bc587c1a1138f045daee81476a82b288c7ab8c7cd6b14bf2b1c7"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8+"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin docs lib src]
+    (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gradle --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Abo Closejdk
+
+## How do I install these formulae?
+`brew install abo/closejdk/<formula>`
+
+Or `brew tap abo/closejdk` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/abo/homebrew-closejdk/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
